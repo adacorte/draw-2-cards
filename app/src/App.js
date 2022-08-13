97eaa8d2e470d5b793108d7842f6bec7e753738d
adacorte <98782641+adacorte@users.noreply.github.com>
2022-08-13 16:21:37 -0400

    modifique el texto principal del landing page

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Busca la Zanahoria!!
         </a>
       </header>
     </div>
